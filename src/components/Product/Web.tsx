8bdd5c324869faa11569ba5bece25c5f6f01445b
SrMoffat <ngigemoffat@gmail.com>
2021-06-09 12:11:53 +0300

    fix product image bug

--- src/components/Product/Web.tsx
+++ src/components/Product/Web.tsx
@@ -1,67 +1,72 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import { get } from 'lodash';
-import { useHistory } from "react-router-dom";
-import { MapPin, Briefcase } from "react-feather";
-import { Flex, Image, Text, Button, Grid, Tag } from '@chakra-ui/core';
+import { get } from 'lodash'
+import { useHistory } from 'react-router-dom'
+import { MapPin, Briefcase } from 'react-feather'
+import { Flex, Image, Text, Button, Grid, Tag } from '@chakra-ui/core'
 
-import Input from '../../components/Input';
-import Section from '../../components/Section';
-import ProductCard from '../../components/Card/ProductCard';
+import Input from '../../components/Input'
+import Section from '../../components/Section'
+import ProductCard from '../../components/Card/ProductCard'
 
-import { ProductProps } from "./props";
-import { images, theme } from "../../theme";
-import { Product } from "../../generated/graphql";
-import { useAppContext } from '../../context/AppProvider';
-import { VerifiedBadge } from "../../components/Product";
+import { ProductProps } from './props'
+import { images, theme } from '../../theme'
+import { Product } from '../../generated/graphql'
+import { useAppContext } from '../../context/AppProvider'
+import { VerifiedBadge } from '../../components/Product'
 
-const ProductComponent: React.FC<ProductProps> = (
-  {
-    deals,
-    product,
-    isPreview,
-    productImages,
-    productPackaging,
-    handleAddToCartClicked,
-    handleAddToWishlistClicked,
-  }) => {
+const ProductComponent: React.FC<ProductProps> = ({
+  deals,
+  product,
+  isPreview,
+  productImages,
+  productPackaging,
+  handleAddToCartClicked,
+  handleAddToWishlistClicked
+}) => {
   const history = useHistory()
-  const { drawerOpen } = useAppContext();
+  const { drawerOpen } = useAppContext()
 
   const navigateToProduct = (id: string | undefined) => {
     history.push(`/product/${id}`)
   }
 
-  const coverImage = product?.coverImage?.url;
-  const hasProductImages = productImages?.length > 0;
+  const coverImage = product?.coverImage?.url
+  const hasProductImages = productImages?.length > 0
 
-  const maxSellCost = get(product, 'maxSellCost') as number;
-  const tradeFedCost = get(product, 'tradeFedCost') as number;
-  const discount = Math.round(((maxSellCost - tradeFedCost) / maxSellCost) * 100);
+  const maxSellCost = get(product, 'maxSellCost') as number
+  const tradeFedCost = get(product, 'tradeFedCost') as number
+  const discount = Math.round(((maxSellCost - tradeFedCost) / maxSellCost) * 100)
 
-  const addresses = get(product, 'business.address');
+  const addresses = get(product, 'business.address')
 
-  const businessAddress = addresses ? addresses[0]?.address : '';
+  const businessAddress = addresses ? addresses[0]?.address : ''
 
-  const textColor = "#355EC0";
-  const coverImageWidth = drawerOpen ? '380px' : '450px';
+  const textColor = '#355EC0'
+  const coverImageWidth = drawerOpen ? '415px' : '450px'
+  const coverImageHeight = drawerOpen ? '435px' : '435px'
 
   return (
     <React.Fragment>
-      <Flex mb={3} backgroundColor="white" borderRadius={5} width="80%" boxShadow={theme.boxShadowMedium}>
+      <Flex
+        mb={3}
+        backgroundColor="white"
+        borderRadius={5}
+        width="80%"
+        boxShadow={theme.boxShadowMedium}
+      >
         <Grid gridTemplateColumns="1fr 1fr">
-          <Grid gridTemplateColumns={`${coverImageWidth} 150px`} height={435} columnGap={3} >
-            <Flex
-              m={5}
-              width={`${coverImageWidth}`}
-              height="450px"
-              position="relative"
-            >
+          <Grid
+            gridTemplateColumns={`${coverImageWidth} 150px`}
+            height={coverImageHeight}
+            columnGap={3}
+          >
+            <Flex m={5} width={`${coverImageWidth}`} height="450px" position="relative">
               <Image
-                width="100%"
-                height="100%"
+                width={coverImageWidth}
+                height={coverImageHeight}
                 src={coverImage || ''}
-                objectFit="contain"
+                objectFit="cover"
               />
               {discount ? (
                 <Flex
@@ -86,23 +91,31 @@ const ProductComponent: React.FC<ProductProps> = (
                 </Flex>
               ) : null}
             </Flex>
-            {hasProductImages
-              && (
-                <Grid gridTemplateRows="90px 90px 90px 90px" rowGap={3} ml={5} mt={6} overflowY="scroll">
-                  {
-                    productImages?.map((product: string | undefined) => (
-                      <Image
-                        key={product || `${Math.random()}`}
-                        width="90%"
-                        height="90px"
-                        src={product || ''}
-                      />
-                    ))
-                  }
-                </Grid>)
-            }
+            {hasProductImages && (
+              <Grid
+                gridTemplateRows="90px 90px 90px 90px"
+                rowGap={3}
+                ml={5}
+                mt={6}
+                overflowY="scroll"
+              >
+                {productImages?.map((product: string | undefined) => (
+                  <Image
+                    key={product || `${Math.random()}`}
+                    width="90%"
+                    height="90px"
+                    objectFit="cover"
+                    src={product || ''}
+                  />
+                ))}
+              </Grid>
+            )}
           </Grid>
-          <Grid gridTemplateRows="40px 50px 35px 30px 50px 40px 30px 30px 50px 60px" padding={5} pt={8}>
+          <Grid
+            gridTemplateRows="40px 50px 35px 35px 50px 40px 30px 30px 50px 60px"
+            padding={5}
+            pt={8}
+          >
             <Text fontSize="20px" fontWeight={600}>
               {product?.name}
             </Text>
@@ -113,7 +126,7 @@ const ProductComponent: React.FC<ProductProps> = (
             <Text mt={3} fontSize="24px" fontWeight={600}>
               {`${product?.currency} ${product?.tradeFedCost}.00`}
             </Text>
-            <Text mt={3} fontSize="16px">
+            <Text mt={4} fontSize="16px">
               {`Retail: ${product?.currency} ${product?.maxSellCost}.00`}
             </Text>
             <Text mt={3} fontSize="14px" color={textColor} fontWeight={600}>
@@ -121,20 +134,21 @@ const ProductComponent: React.FC<ProductProps> = (
             </Text>
             <Flex mt={3}>
               <Briefcase />
-              <Text ml={3} fontSize="14px" >
+              <Text ml={3} fontSize="14px">
                 {`Supplied by ${product?.business?.name}`}
               </Text>
             </Flex>
-            {
-              businessAddress && (
-                <Flex mt={3}>
-                  <MapPin />
-                  <Text ml={3} fontSize="14px" >
-                    {`Delivered from ${product?.business && product?.business?.address && product?.business?.address[0]?.address}`}
-                  </Text>
-                </Flex>
-              )
-            }
+            {businessAddress && (
+              <Flex mt={3}>
+                <MapPin />
+                <Text ml={3} fontSize="14px">
+                  {`Delivered from ${product?.business &&
+                    product?.business?.address &&
+                    product?.business?.address[0]?.address
+                    }`}
+                </Text>
+              </Flex>
+            )}
             <Flex mt={3}>
               <Input
                 name="quantity"
@@ -143,86 +157,130 @@ const ProductComponent: React.FC<ProductProps> = (
                 style={{
                   padding: 3,
                   border: `1px solid ${theme.colors.background}`,
-                  width: "40%",
-                  marginTop: "15px"
+                  width: '40%',
+                  marginTop: '15px'
                 }}
               />
-              <Text mt={5} ml={3} color={textColor} fontSize={12} fontWeight={600}>{`${product?.availableUnits} units`}</Text>
+              <Text
+                mt={5}
+                ml={3}
+                color={textColor}
+                fontSize={12}
+                fontWeight={600}
+              >{`${product?.availableUnits} units`}</Text>
               <Text mt={5} ml={1} fontSize={12}>{`available`}</Text>
             </Flex>
-            <Flex flexWrap="wrap" >
-              <Button justifySelf="start" width="150px" mt={4} mr={2} onClick={() => handleAddToWishlistClicked(product?.id)} border={`1px solid ${theme.colors.brand[500]}`} background="white">
-                <Text fontSize="12px">
-                  ADD TO WISHLIST
-                </Text>
+            <Flex flexWrap="wrap">
+              <Button
+                justifySelf="start"
+                width="150px"
+                mt={4}
+                mr={2}
+                onClick={() => handleAddToWishlistClicked(product?.id)}
+                border={`1px solid ${theme.colors.brand[500]}`}
+                background="white"
+              >
+                <Text fontSize="12px">ADD TO WISHLIST</Text>
               </Button>
-              <Button mt={4} variantColor="brand" width="150px" onClick={() => handleAddToCartClicked(product?.id)}>
-                <Text fontSize="12px">
-                  ADD TO CART
-                </Text>
+              <Button
+                mt={4}
+                variantColor="brand"
+                width="150px"
+                onClick={() => handleAddToCartClicked(product?.id)}
+              >
+                <Text fontSize="12px">ADD TO CART</Text>
               </Button>
             </Flex>
           </Grid>
         </Grid>
       </Flex>
-      <Grid gridTemplateRows="1fr 1fr" backgroundColor="white" borderRadius={3} padding={10} fontSize="12px" width="80%" boxShadow={theme.boxShadowMedium}>
+      <Grid
+        gridTemplateRows="1fr 1fr"
+        backgroundColor="white"
+        borderRadius={3}
+        padding={10}
+        fontSize="12px"
+        width="80%"
+        boxShadow={theme.boxShadowMedium}
+      >
         <div>
           <Text fontSize="17px" fontWeight={600}>
             About This Product
           </Text>
-          <Text fontSize="12px">
-            {product?.description}
-          </Text>
+          <Text fontSize="12px">{product?.description}</Text>
         </div>
         <Grid gridTemplateColumns="1fr 1fr" columnGap="20px">
-          <div style={{ border: `1px solid ${theme.colors.background}`, padding: 15, borderRadius: 3, }}>
+          <div
+            style={{ border: `1px solid ${theme.colors.background}`, padding: 15, borderRadius: 3 }}
+          >
             <Text fontSize="17px" fontWeight={600} mb={3}>
               Product Features
             </Text>
             <ul style={{ marginLeft: 15 }}>
-              {
-                product?.features?.split(",")?.map((feature: string, index: number) => (
-                  <li key={`${feature}_${index}`}>
-                    <Text fontSize="12px">{feature}</Text>
-                  </li>
-
-                ))
-              }
+              {product?.features?.split(',')?.map((feature: string, index: number) => (
+                <li key={`${feature}_${index}`}>
+                  <Text fontSize="12px">{feature}</Text>
+                </li>
+              ))}
             </ul>
           </div>
-          <div style={{ border: `1px solid ${theme.colors.background}`, padding: 15, borderRadius: 3, }}>
+          <div
+            style={{ border: `1px solid ${theme.colors.background}`, padding: 15, borderRadius: 3 }}
+          >
             <Text fontSize="17px" fontWeight={600} mb={3}>
               Product Specification
             </Text>
             <Flex justify="space-between">
               <Flex flexDirection="column">
-                <Text fontSize="12px">Height : {`${product?.height} ${product?.dimensionsUnit}`}</Text>
-                <Text fontSize="12px">Width : {`${product?.width} ${product?.dimensionsUnit}`}</Text>
-                <Text fontSize="12px">Length : {`${product?.lengths} ${product?.dimensionsUnit}`}</Text>
+                <Text fontSize="12px">
+                  Height : {`${product?.height} ${product?.dimensionsUnit}`}
+                </Text>
+                <Text fontSize="12px">
+                  Width : {`${product?.width} ${product?.dimensionsUnit}`}
+                </Text>
+                <Text fontSize="12px">
+                  Length : {`${product?.lengths} ${product?.dimensionsUnit}`}
+                </Text>
                 <Text fontSize="12px">Weight : {`${product?.weight} ${product?.weightUnit}`}</Text>
               </Flex>
-              {
-                product?.additionalInfo && (
-                  <a target="_blank" rel="noopener noreferrer" href={product?.additionalInfo[0]?.url || ''}>
-                    <Flex ml={5} justify="space-between" border={theme.colors.background} borderRadius={3}>
-                      <Image src={images.pdfFile} height="40px" />
-                      <Text ml={2} mt={2}>Additional Product Information</Text>
-                    </Flex>
-                  </a>
-                )
-              }
+              {product?.additionalInfo && (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={product?.additionalInfo[0]?.url || ''}
+                >
+                  <Flex
+                    ml={5}
+                    justify="space-between"
+                    border={theme.colors.background}
+                    borderRadius={3}
+                  >
+                    <Image src={images.pdfFile} height="40px" />
+                    <Text ml={2} mt={2}>
+                      Additional Product Information
+                    </Text>
+                  </Flex>
+                </a>
+              )}
             </Flex>
           </div>
           <Flex width="414px" background={theme.colors.accent[50]} mt={5}>
-            {
-              product?.tags?.split(",")?.map((tag: string, index: number) => (
-                <Tag fontSize={12} mr={1} size="sm" key={index} background={theme.colors.tag} color={theme.colors.tagText}>{tag?.toUpperCase()}</Tag>
-              ))
-            }
+            {product?.tags?.split(',')?.map((tag: string, index: number) => (
+              <Tag
+                fontSize={12}
+                mr={1}
+                size="sm"
+                key={index}
+                background={theme.colors.tag}
+                color={theme.colors.tagText}
+              >
+                {tag?.toUpperCase()}
+              </Tag>
+            ))}
           </Flex>
         </Grid>
       </Grid>
-      {isPreview &&
+      {isPreview && (
         <Flex ml={5} mt={3} width="100%" flexDirection="column" alignItems="center">
           <Section card title="Deals You Might Be Interested In">
             {deals?.map((product: Product) => (
@@ -230,12 +288,10 @@ const ProductComponent: React.FC<ProductProps> = (
             ))}
           </Section>
           <Button width="80%" variantColor="brand">
-            <Text fontSize="12px">
-              VIEW MORE
-            </Text>
+            <Text fontSize="12px">VIEW MORE</Text>
           </Button>
         </Flex>
-      }
+      )}
     </React.Fragment>
   )
 }
