fdba42ffb9ce9874caa18dccaa70359066942965
SrMoffat <ngigemoffat@gmail.com>
2021-06-25 17:09:27 +0300

    feat(productManagement): add charts  - add charts

--- src/components/Card/ProductMangementCard/ActiveProgress.tsx
+++ src/components/Card/ProductMangementCard/ActiveProgress.tsx
@@ -1,11 +1,74 @@
 import * as React from 'react'
 
+import Chart from 'react-apexcharts'
+
 import { Flex, FlexProps } from '@chakra-ui/core'
+
+import { ACTIVE_PRODUCT_PROGRESS } from '../../../constants'
 
 type ActiveProgressCardProps = FlexProps & {}
 
 const ActiveProgressCard: React.FC<ActiveProgressCardProps> = () => {
-  return <Flex>ActiveProgress Chart</Flex>
+  const options = {
+    chart: {
+      id: ACTIVE_PRODUCT_PROGRESS
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        dataLabels: {
+          position: 'top'
+        }
+      }
+    },
+    xaxis: {
+      categories: [
+        'BMW',
+        'Hammer',
+        'Jeep',
+        'Tesla',
+        'Land Rover',
+        'Land Cruiser',
+        'Lamborghini',
+        'Mercedes Benz',
+        'Jaguar',
+        'Porsche',
+        'Toyota',
+        'Hyundai'
+      ]
+    },
+    annotations: {
+      xaxis: [
+        {
+          x: 80,
+          x2: 100,
+          fillColor: '#9fcdff',
+          label: {
+            text: 'Hotcake (Almost Out)'
+          }
+        }
+      ]
+    },
+    fill: {
+      colors: ['#3ACF2F', '#74a970', '#9C27B0']
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: (val: string) => `${val}%`,
+      offsetX: -10
+    }
+  }
+  const series = [
+    {
+      name: ACTIVE_PRODUCT_PROGRESS,
+      data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    }
+  ]
+  return (
+    <Flex>
+      <Chart options={options} series={series} type="bar" width={500} height={320} />
+    </Flex>
+  )
 }
 
 export default ActiveProgressCard
--- src/components/Card/ProductMangementCard/TotalUnits.tsx
+++ src/components/Card/ProductMangementCard/TotalUnits.tsx
@@ -1,11 +1,53 @@
 import * as React from 'react'
 
+import Chart from 'react-apexcharts'
+
 import { Flex, FlexProps } from '@chakra-ui/core'
+
+import { TOTAL_UNITS_SOLD } from '../../../constants'
 
 type TotalUnitsCardProps = FlexProps & {}
 
 const TotalUnitsCard: React.FC<TotalUnitsCardProps> = () => {
-  return <Flex>Total Units Chart</Flex>
+  const options = {
+    chart: {
+      id: TOTAL_UNITS_SOLD
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          position: 'top'
+        }
+      }
+    },
+    xaxis: {
+      categories: [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dev'
+      ]
+    }
+  }
+  const series = [
+    {
+      name: TOTAL_UNITS_SOLD,
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }
+  ]
+  return (
+    <Flex>
+      <Chart options={options} series={series} type="bar" width={500} height={320} />
+    </Flex>
+  )
 }
 
 export default TotalUnitsCard
