255a1c5e5ccb15d88d277e22ae04deefad6e69bb
refiloematlapeng <matlapengrp@gmail.com>
2021-09-13 03:38:09 +0200

    SignUp flow (create an account)

--- src/containers/Register/index.tsx
+++ src/containers/Register/index.tsx
@@ -5,7 +5,26 @@ import { Mail } from 'react-feather'
 import { ApolloError } from 'apollo-boost'
 import { Form, Formik, FormikProps } from 'formik'
 import { Link, useHistory } from 'react-router-dom'
-import { Button, Flex, Image, useToast, Tabs, TabList, Tab, Box } from '@chakra-ui/core'
+import {
+  Button,
+  Flex,
+  Image,
+  useToast,
+  Tabs,
+  TabList,
+  Tab,
+  Box,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/core'
 
 import Input from '../../components/Input'
 
@@ -50,6 +69,8 @@ const Register: React.FC<RegisterProps> = () => {
 
   const history = useHistory()
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   React.useEffect(() => {
     if (user?.confirmed) {
       history.push('/')
@@ -66,6 +87,7 @@ const Register: React.FC<RegisterProps> = () => {
   }
   const termsAndConditionsLink = legalities?.legality?.termsAndConditionsFile?.url
   return (
+    // Login illustration image
     <PageWrap
       align="center"
       title="Login"
@@ -76,127 +98,156 @@ const Register: React.FC<RegisterProps> = () => {
       pt={0}
     >
       <SideSlider>
-        {/* <Box p="4" bg="#FFFAFA"> */}
+        {/** Business account register */}
         <Flex width="100%" flexDirection="column" pb={4} color="white">
           <H3 textAlign="center" mb={4} fontWeight="bold" color={theme.colors.brand[500]}>
-            Register
+            Hi there! Welcome to TradeFed.
           </H3>
           <Text textAlign="center" fontSize="18px" color="gray.500">
-            Hi there! Welcome to TradeFed.
+            Choose an account type.
           </Text>
         </Flex>
-        <Tabs isFitted variant="enclosed" defaultIndex={1}>
-          <TabList>
-            <Tab _focus={{ borderColor: 'white' }} _selected={{ bg: 'gray.300' }}>
-              Business
-            </Tab>
-            <Tab _focus={{ borderColor: 'white' }} _selected={{ bg: 'gray.300' }}>
-              Individual
-            </Tab>
-          </TabList>
-        </Tabs>
-        <Box p="4" bg="gray.300">
-          <Formik
-            validationSchema={RegisterFormValidation}
-            initialValues={{
-              email: '',
-              password: ''
-            }}
-            onSubmit={async ({ email, password }, { setStatus, setSubmitting }) => {
-              if (!termsChecked) {
-                setShowError(true)
-                return
-              } else {
-                setStatus(null)
-                try {
-                  setSubmitting(true)
-                  if (register) {
-                    await register(email, password)
-                  }
-                  setSubmitting(false)
-                } catch (error) {
-                  setStatus(formatError(error))
-                }
-              }
-            }}
-          >
-            {({ isSubmitting, status }: FormikProps<RegisterValues>) => (
-              <Form style={{ width: '100%' }}>
-                <ConnectedFormGroup icon={Mail} name="email" placeholder="Email" type="email" />
-                <ConnectedPasswordGroup name="password" placeholder="Password" />
-                {status && (
-                  <MotionFlex initial={{ opacity: 0 }} animate={{ opacity: 1 }} mb={2} width="100%">
-                    <Text textAlign="right" color="red.500">
-                      {status}
-                    </Text>
-                  </MotionFlex>
-                )}
-                <Flex mb={5}>
-                  <Flex mt={5} mr={3}>
-                    <Input type="checkbox" name="terms" onChange={handleTermsCheckboxClicked} />
-                  </Flex>
-                  <Flex
-                    mb={2}
-                    mt={4}
-                    align="center"
-                    justify="center"
-                    color={theme.colors.brand[500]}
-                  >
-                    <Text>
-                      I agree to the{' '}
-                      <a
-                        style={{ fontWeight: 600, textDecoration: 'underline' }}
-                        href={termsAndConditionsLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {terms}
-                      </a>
-                    </Text>
-                  </Flex>
-                </Flex>
-                {showError && !termsChecked && (
-                  <Text fontSize={12} color="red.500">
-                    Kindly read and accept the terms and conditions
-                  </Text>
-                )}
-                <Button
-                  mt={4}
-                  width="100%"
-                  type="submit"
-                  variantColor="brand"
-                  isLoading={isSubmitting}
-                >
-                  SIGN UP
-                </Button>
-                <Flex mb={2} mt={4} align="center" justify="center" flexDirection="column">
-                  <Text my={4} fontSize="14px">
-                    Or continue with social media
-                  </Text>
-                  <Flex width="100%" justifyContent="space-evenly" my={5}>
-                    <a href={`${baseUrl}/connect/facebook`}>
-                      <Image src={images['Facebook']} />
-                    </a>
-                    <a href={`${baseUrl}/connect/linkedin`}>
-                      <Image src={images['LinkedIn']} />
-                    </a>
-                    <a href={`${baseUrl}/connect/google`}>
-                      <Image src={images['GooglePlus']} />
-                    </a>
-                  </Flex>
-                </Flex>
-                <Flex mb={2} mt={4} align="center" justify="center">
-                  <Text>
-                    Already have an account?{' '}
-                    <Link style={{ fontWeight: 600 }} to="/login">
-                      Login
-                    </Link>{' '}
-                  </Text>
-                </Flex>
-              </Form>
-            )}
-          </Formik>
+        <Box
+          onClick={onOpen}
+          as="button"
+          mt="5"
+          boxShadow="lg"
+          height="200px"
+          width="350px"
+          borderRadius="10px"
+          textAlign="center"
+          bg="gray.50"
+        >
+          <Text textAlign="center" fontSize="18px" color="black">
+            Business
+          </Text>
+          <Image
+            marginLeft="150px"
+            marginTop="5px"
+            marginBottom="5px"
+            h="50px"
+            w="50px"
+            objectFit="cover"
+            src="https://www.iconpacks.net/icons/2/free-store-icon-2017-thumb.png"
+          />
+          <Text textAlign="center" fontSize="15px" color="gray.500">
+            For business owners, enterprises, SMEs buyers.
+          </Text>
         </Box>
+        {/** Business account register - MODAL */}
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create your account</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>First name</FormLabel>
+                <Input placeholder="First name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Last name</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Phone number</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Email address</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>City</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button mr={3}>Save</Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        {/** Individual account register */}
+        <Box
+          onClick={onOpen}
+          as="button"
+          mt="5"
+          boxShadow="lg"
+          height="200px"
+          width="350px"
+          borderRadius="10px"
+          textAlign="center"
+          bg="gray.50"
+        >
+          <Text textAlign="center" fontSize="18px" color="black">
+            Individual
+          </Text>
+          <Image
+            marginLeft="150px"
+            marginTop="5px"
+            marginBottom="5px"
+            h="50px"
+            w="50px"
+            objectFit="cover"
+            src="https://www.iconpacks.net/icons/2/free-store-icon-2017-thumb.png"
+          />
+          <Text textAlign="center" fontSize="15px" color="gray.500">
+            For individual buyers.
+          </Text>
+        </Box>
+        {/** Individual account register - MODAL */}
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create your account</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>First name</FormLabel>
+                <Input placeholder="First name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Last name</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Phone number</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Email address</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>City</FormLabel>
+                <Input placeholder="Last name" />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button mr={3}>Save</Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        <Flex mb={2} mt={4} align="center" justify="center">
+          <Text>
+            Already have an account?{' '}
+            <Link style={{ fontWeight: 600 }} to="/login">
+              Login
+            </Link>{' '}
+          </Text>
+        </Flex>
       </SideSlider>
     </PageWrap>
   )
