e21bdd7fbf67c8a2d3de10a808102b4f1db887b3
refiloematlapeng <matlapengrp@gmail.com>
2021-10-12 16:04:07 +0200

    Cancel button

--- src/containers/ProductFilter/index.tsx
+++ src/containers/ProductFilter/index.tsx
@@ -11,6 +11,7 @@ import { MotionFlex } from '../../components'
 import { PageWrap } from '../../layouts'
 import { useHistory } from 'react-router-dom'
 import { useMediaQuery } from 'react-responsive'
+import { theme } from '../../theme'
 
 const ProductFormValidation = Yup.object().shape({
   minPrice: Yup.string(),
@@ -128,8 +129,27 @@ const ProductFilter: React.FC = () => {
                 </Text>
               </MotionFlex>
             )}
-            <Button mt={4} width="100%" type="submit" variantColor="brand" isLoading={isSubmitting}>
-              APPLY
+            <Button
+              ml={1}
+              mt={4}
+              variantColor="brand"
+              type="submit"
+              isLoading={isSubmitting}
+              width="250px"
+            >
+              <Text>APPLY</Text>
+            </Button>
+            <Button
+              mt={4}
+              ml={8}
+              width="250px"
+              // onClick={() => {
+              //   history.push('/profile')
+              // }}
+              border={`1px solid ${theme.colors.brand[700]}`}
+              background="white"
+            >
+              <Text>CANCEL</Text>
             </Button>
           </Form>
         )}
