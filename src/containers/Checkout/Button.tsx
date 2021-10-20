9d9a8f8dc074d4826b70e359f42ea89e0ab9c47d
Sanvir Dessai <sanvir@sovtech.com>
2021-10-20 12:13:35 +0200

    fix(menu): dynamic hamburger icon styling - also includes linting updates

--- src/containers/Checkout/Button.tsx
+++ src/containers/Checkout/Button.tsx
@@ -1,28 +1,35 @@
-import * as React from "react";
+import * as React from 'react'
 
-import { Button } from "@chakra-ui/core";
+import { Button } from '@chakra-ui/core'
 
 type StepButtonProps = {
-    type?: "button" | "submit" | undefined
-    active: number
-    disabled: boolean
-    setActive: (step: number) => void
-};
+  type?: 'button' | 'submit' | undefined
+  active: number
+  disabled: boolean
+  setActive: (step: number) => void
+}
 
-const StepButton: React.FC<StepButtonProps> = ({ children, setActive, active, disabled, type, ...rest }) => {
-    return (
-        <Button
-            mt={5}
-            width="100%"
-            type={type ? type : "button"}
-            variantColor="brand"
-            variant="solid"
-            isDisabled={disabled}
-            onClick={() => setActive(active + 1)}
-            {...rest}
-        >
-            { children }
-        </Button>
-    );
-};
-export default StepButton;
+const StepButton: React.FC<StepButtonProps> = ({
+  children,
+  setActive,
+  active,
+  disabled,
+  type,
+  ...rest
+}) => {
+  return (
+    <Button
+      mt={5}
+      width="100%"
+      type={type ? type : 'button'}
+      variantColor="brand"
+      variant="solid"
+      isDisabled={disabled}
+      onClick={() => setActive(active + 1)}
+      {...rest}
+    >
+      {children}
+    </Button>
+  )
+}
+export default StepButton
