cef9f0a052faf4ed922721c7febc0c0390f8326e
refiloematlapeng <matlapengrp@gmail.com>
2021-11-05 12:25:43 +0200

    fix: clickable external link

--- src/components/InfoBanner/index.tsx
+++ src/components/InfoBanner/index.tsx
@@ -1,19 +1,17 @@
 import { Flex, Text } from '@chakra-ui/core'
 import * as React from 'react'
 import { ColorProps } from 'styled-system'
-import { useMediaQuery } from 'react-responsive'
 import { Link } from 'react-router-dom'
 
 type InfoBannerProps = ColorProps & {
   caption?: string
 }
 
-const InfoBanner: React.FC<InfoBannerProps> = ({ caption }) => {
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 40em)' })
+const InfoBanner: React.FC<InfoBannerProps> = ({}) => {
   return (
     <Flex
       width="100vw"
-      minHeight={isTabletOrMobile ? '5px' : '5px'}
+      minHeight={10}
       bg="brand.700"
       mr="16px"
       flexDirection="column"
@@ -21,9 +19,8 @@ const InfoBanner: React.FC<InfoBannerProps> = ({ caption }) => {
     >
       <Text fontSize={13} mt={3} color="accent.50">
         <Link to={{ pathname: 'https://www.sacoronavirus.co.za' }} target="_blank">
-          For official Government information about COVID-19, please visit
-        </Link>
-        {' www.sacoronavirus.co.za'}
+          For official Government information about COVID-19, please visit www.sacoronavirus.co.za
+        </Link>{' '}
       </Text>
     </Flex>
   )
