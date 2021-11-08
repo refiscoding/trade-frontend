4f9c7a15c6da920d22f9b2505dd6181c1d7b25a7
refiloematlapeng <matlapengrp@gmail.com>
2021-11-08 14:16:58 +0200

    include product image

--- src/containers/Checkout/ReceiptProduct.tsx
+++ src/containers/Checkout/ReceiptProduct.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { Grid, Flex, Tag } from '@chakra-ui/core'
+import { Grid, Flex, Tag, Image } from '@chakra-ui/core'
 import { Text } from '../../typography'
 import { theme } from '../../theme'
 
@@ -21,7 +21,8 @@ const ReceiptProductComponent: React.FC<ReceiptProductComponentProps> = ({
     <Flex height="70px" alignSelf="start" borderBottom={`1px solid ${theme.colors.background}`}>
       <Grid width="100%" gridTemplateColumns="50% 50%" justifyContent="space-between">
         <Flex justifySelf="start">
-          <Text fontWeight={600} fontSize={14}>
+          <Image width="50px" height="50px" src={product?.coverImage?.url || ''} />
+          <Text ml={10} fontWeight={600} fontSize={14}>
             {product?.name}
           </Text>
           <Tag
